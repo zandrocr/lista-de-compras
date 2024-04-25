8f60ca0f3224225ea3fbe48f3066e6d03ef0dc77
zandrocr <alezandrocosta@live.com>
2024-04-24 23:07:53 -0300

    update: error

--- src/components/search-bar/index.jsx
+++ src/components/search-bar/index.jsx
@@ -2,11 +2,11 @@ import { useState } from "react"
 import { Input } from "../input"
 
 export function SearchBar() {
-    const [array, setArray] = useState()
-    
+	const [array, setArray] = useState()
+
 	return (
 		<div>
-			<Input placeholder='Digite o produto que deseja'/>
+			<Input placeholder="Digite o produto que deseja" />
 		</div>
 	)
 }
