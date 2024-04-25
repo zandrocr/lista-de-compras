e06270dbcc248b6cffd53fec08c0bb6f5d419fb2
zandrocr <alezandrocosta@live.com>
2024-04-24 21:59:34 -0300

    commit l

--- src/components/search-bar/index.jsx
+++ src/components/search-bar/index.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react"
 import { Input } from "../input"
 
 export function SearchBar() {
+    const [array, setArray] = useState()
+    
 	return (
 		<div>
 			<Input placeholder='Digite o produto que deseja'/>
